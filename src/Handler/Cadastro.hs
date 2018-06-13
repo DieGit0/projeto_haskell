741e3cd742d8d52693fc39ee63a2b13f8c2ec2ce
aldo30 <genivaldosantana32@gmail.com>
2018-06-12 23:22:26 -0300

    Criacao metodo post Cadastro, insercao de usuario no banco de dados.

--- src/Handler/Cadastro.hs
+++ src/Handler/Cadastro.hs
@@ -16,9 +16,22 @@ cadastroPostForm = renderDivs $ User
                   <*> areq emailField (bfs ("email"::Text)) Nothing 
                   <*> areq passwordField (bfs ("senha"::Text)) Nothing 
                   <*> areq dayField (bfs ("aniversário"::Text)) Nothing
-                    
+
 getCadastrarR:: Handler Html
 getCadastrarR = do
      (widget,enctype) <- generateFormPost $ cadastroPostForm
      defaultLayout $ do
         $(widgetFile "cadastrar")
+
+postCadastrarR:: Handler Html
+postCadastrarR = do
+    ((res,_),_) <- runFormPost cadastroPostForm
+    case res of
+        FormSuccess user -> do
+            uid <- runDB $ insert user
+            defaultLayout $ do 
+               [whamlet|
+                    <h1>
+                        User #{fromSqlKey uid} inserido(a) com sucesso!  
+               |]
+        _ -> redirect HomeR        
